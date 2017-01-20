e802f4ce5e69d270ac05f33679454591365feb32
YUNAZ\Yunaz Gilang <yunaz.gilang@gmail.com>
2017-01-20 13:47:39 +0700

    Fix the update story (example)

--- src/test/java/com/blibli/test/steps/UpdateTheProfile.java
+++ src/test/java/com/blibli/test/steps/UpdateTheProfile.java
@@ -2,6 +2,7 @@ package com.blibli.test.steps;
 
 import com.blibli.test.steps.actor.User;
 import net.thucydides.core.annotations.Steps;
+import org.jbehave.core.annotations.When;
 
 /**
  * Created by Yunaz on 1/20/2017.
@@ -10,5 +11,13 @@ public class UpdateTheProfile {
     @Steps
     User actor;
 
+    @When("The user want to update the profile")
+    public void user_is_open_update_profile_page(){
 
+    }
+
+    @When("The user is inserting his fullname : '$fullname'")
+    public void user_is_inserting_his_fullname(String fullname){
+
+    }
 }
--- src/test/resources/stories/update_profile_attempt/UpdateStoryTest.story
+++ src/test/resources/stories/update_profile_attempt/UpdateStoryTest.story
@@ -6,6 +6,7 @@ I want to update my profile
 So that I can set detailed information about my profile
 
 Scenario: scenario description
-Given
-When I do something
-Then system is in a different state
+Given The user is on the 'http://blibli.com'
+Then The user should be signed in and redirected to the website
+When The user want to update the profile
+When The user is inserting his fullname : 'Yunaz Gilang'
